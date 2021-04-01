2a3fcf44dba0ef7c960504f2a65ee06a1caf1595
Alexey Mikailov <79835478+AxelKroos@users.noreply.github.com>
2021-04-01 19:45:04 +0300

    Add files via upload

---
+++ Шарики/index.js
@@ -0,0 +1,101 @@
+let field = document.querySelector('#field');
+let img = document.querySelector('img');
+let array = [];
+let omitted = 0;
+
+/*Каждый шарик имеет свою текущую координату, которая изменяется*/
+let oldCoordsY = [];
+let oldCoordsX = [];
+
+/*Счётчик попаданий*/
+window.score.innerHTML = '0';
+let currentScore = 0;
+
+/*Создаём обработчик события для движения иголки за курсором мыши по горизонтали*/
+field.addEventListener('mousemove', () => {
+    img.style.right = 625 - event.clientX + 'px';
+    field.style.cursor = 'none';
+})
+
+/*Задаём скорость движения шариков и изменяем его каждые n секунд*/
+let speed = 1;
+setInterval(() => {speed += 0.5}, 5000)
+
+/*Создаём массив с координатами*/
+let arr = [];
+for (let i = 1; i < 40; i++) {
+    arr.push(`${i * 20}px`)
+}
+
+/*Создаём функцию рандомно выбирающую координату*/
+function arrayRandElement(arr) {
+    let rand = Math.floor(Math.random() * arr.length);
+    return arr[rand];
+}
+
+/*Создаём шарики и задаём начальную позицию*/
+for (let i = 0; i < 1000; i++) {
+    let airBaloon = document.createElement('div');
+    airBaloon.id = `airBaloon${i}`;
+    array.push(airBaloon.id);
+    airBaloon.classList.add('airBaloon');
+    airBaloon.style.top = '400px';
+    airBaloon.style.left = arrayRandElement(arr);
+    field.appendChild(airBaloon);
+    oldCoordsY.push(airBaloon.getBoundingClientRect().top);
+    oldCoordsX.push(airBaloon.getBoundingClientRect().right);
+
+
+    /*Интервально изменяем положение шарика*/
+    setInterval(() => {
+        if (oldCoordsY[i] > -40) {
+            oldCoordsY[i] = oldCoordsY[i] - speed;
+            airBaloon.style.top = oldCoordsY[i] + 'px';
+        } else {
+            airBaloon.style.top = oldCoordsY[i]
+        }
+
+/*Условие, при котором проверяется попадание кончика игры в шарик. В ходе тестирования пришел к выводу,
+            что такое мудрёное условие проверки самое подходящее*/
+        if (oldCoordsY[i] < 50 && oldCoordsY[i] > 40) {
+            if (oldCoordsX[i] > img.getBoundingClientRect().right - 15 && oldCoordsX[i] < img.getBoundingClientRect().right + 15) {
+                oldCoordsY[i] = -50;
+                airBaloon.style.display = 'none';
+                window.score.innerHTML = +window.score.innerHTML + 1;
+                currentScore++;
+            }
+        }
+
+        airBaloon.style.position = 'absolute';
+
+        if (oldCoordsY[i] > -40 && oldCoordsY[i] < -20) {
+            omitted++;
+            oldCoordsY[i] = -100;
+        }
+
+    }, 5);
+}
+
+/*Наш таймер на одну минуту*/
+let timer = document.querySelector('#timer');
+let score = document.querySelector('.score');
+timer.innerHTML = '60';
+setInterval(() => {
+    timer.innerHTML = +timer.innerHTML - 1;
+}, 1000);
+
+/*Конец игры по истечению одной минуты*/
+setTimeout(() => {
+    oldCoordsY = [];
+    timer.style.display = 'none';
+    score.style.left = '23%';
+    score.innerHTML = `Игра окончена. Ваш счёт: ${currentScore} <br> <p style="color: red">Пропущено: ${omitted}<p>`;
+    console.log(omitted)
+}, 60000);
+
+
+
+
+
+
+
